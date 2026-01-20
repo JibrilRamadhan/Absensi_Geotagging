6c5da2fc3920b90788f4f68a77cbfbd33ed4a403
achmad-gg <achmadhabibu@gmail.com>
2026-01-20 18:12:50 +0700

    adalah pokoknya

--- src/views/admin/manage_users.vue
+++ src/views/admin/manage_users.vue
@@ -1,3 +1,47 @@
+<script setup>
+import api from "@/api/axios";
+import { onMounted, ref } from "vue";
+
+const users = ref([]);
+const loading = ref(true);
+
+onMounted(async () => {
+  try {
+    users.value = (await api.get("/api/admin/users/all")).data;
+  } catch (e) {
+    alert(e.message);
+  } finally {
+    loading.value = false;
+  }
+});
+</script>
+
 <template>
-  <h1>manage users</h1>
+  <h2>Data Siswa Magang</h2>
+
+  <p v-if="loading">Loading...</p>
+
+  <table v-else border="1">
+    <thead>
+      <tr>
+        <th>Nama</th>
+        <th>Email</th>
+        <th>Perusahaan</th>
+        <th>Status Hari Ini</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="u in users" :key="u.id">
+        <td>{{ u.name }}</td>
+        <td>{{ u.email }}</td>
+        <td>{{ u.company_name }}</td>
+        <td>
+          <span v-if="u.status">
+            {{ u.status }}
+          </span>
+          <span v-else>-</span>
+        </td>
+      </tr>
+    </tbody>
+  </table>
 </template>
